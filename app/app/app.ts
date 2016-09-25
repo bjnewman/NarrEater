c1942b3feb42168d6ca69c02397d85283c099967
David Armbrust <david.armbrust@gmail.com>
2016-09-25 11:48:51 -0500

    Update root page to main

--- app/app/app.ts
+++ app/app/app.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { ionicBootstrap, Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 
-import { HomePage } from './pages/home/home';
+import { Main } from './pages/main/main';
 
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+  rootPage: any = Main;
 
   constructor(public platform: Platform) {
     platform.ready().then(() => {
