fdae48d55f97f6a061c5475fb37e00a7a35ccc09
ebrothe2 <eholmes3@gmail.com>
2016-09-25 15:03:53 -0500

    menu list on main page with made with menu models appears on the main page

--- app/app/pages/main/main.ts
+++ app/app/pages/main/main.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MenuView } from '../menuview/menuview';
+import { Menu } from '../../models/menu';
 
 @Component({
   templateUrl: 'build/pages/main/main.html'
 })
 export class Main {
+  menus: any;
   constructor(public navCtrl: NavController) {
-
+    this.menus = [
+      new Menu('menu1', 'content1'),
+      new Menu('menu2', 'content2'),
+      new Menu('menu3', 'content3')
+    ];
+  }
+  menuSelected(menu){
+    this.navCtrl.push(MenuView, {menu: menu});
   }
 }
