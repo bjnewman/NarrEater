d22f19c80b31ce927356e5da388e336fd38b7e3d
ebrothe2 <eholmes3@gmail.com>
2016-09-25 15:50:09 -0500

    clicking on menus take you to that specific menu page

--- app/app/pages/menuview/menuview.ts
+++ app/app/pages/menuview/menuview.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {NavParams} from 'ionic-angular';
+import { Menu } from '../../models/menu';
 
 @Component({
   templateUrl: 'build/pages/menuview/menuview.html'
 })
 export class MenuView {
-  constructor(public navCtrl: NavController) {
+  private title;
+  private content;
 
+  constructor(public navCtrl: NavController, private navParams: NavParams) {
+    this.title = this.navParams.get('menu').title;
+    this.content = this.navParams.get('menu').content;
   }
+
 }
