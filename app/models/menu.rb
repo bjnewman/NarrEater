a3168bb00aa9aa80d3c70378b8315b90be2fa0ee
Ben Newman <email": null>
2016-09-25 15:27:15 -0500

    update paperclip validations for security and size restrictions

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,6 @@
 class Menu < ActiveRecord::Base
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", small: "100x100>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment :image, presence: true,
+  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+  size: { in: 0..10.megabytes}
 end
