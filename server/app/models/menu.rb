924c9190c986539569b5707f73012ed42ad17687
Ben Newman <email": null>
2016-09-26 16:54:34 -0500

    fix menu validation and delete temp images

--- server/app/models/menu.rb
+++ server/app/models/menu.rb
@@ -1,9 +1,9 @@
 class Menu < ActiveRecord::Base
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", small: "100x100>" }
   validates_attachment :image, presence: true,
-  validate :image_dimensions
   content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
   size: { in: 0..10.megabytes}
+  validate :image_dimensions
 
 
   private
