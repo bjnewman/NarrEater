388194b7cc8f4a34fd234c00a990eb200bdec3fb
stevezhere <sfang4@yahoo.com>
2016-09-26 19:16:13 -0500

    refactored post route and fixed image validation

--- server/app/models/menu.rb
+++ server/app/models/menu.rb
@@ -1,10 +1,9 @@
 class Menu < ActiveRecord::Base
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", small: "100x100>" }
   validates_attachment :image, presence: true,
-  validate :image_dimensions
   content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
   size: { in: 0..10.megabytes}
-
+  validate :image_dimensions, on: :create
 
   private
 
@@ -12,8 +11,7 @@ class Menu < ActiveRecord::Base
     required_width  = 1024
     required_height = 768
     dimensions = Paperclip::Geometry.from_file(image.queued_for_write[:original].path)
-
-    errors.add(:image, "Width must be #{width}px") unless dimensions.width >= required_width
-    errors.add(:image, "Height must be #{height}px") unless dimensions.height >= required_height
+    errors.add(:image, "Width must be at least 1024 px") unless dimensions.width >= required_width
+    errors.add(:image, "Height must be at least 768 px") unless dimensions.height >= required_height
   end
 end
