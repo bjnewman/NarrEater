b2d20b7ea02fc6867ed3cb2572a2f53c86cc5e9e
Erica Brotherton <email": null>
2016-09-27 16:13:02 -0500

    added info about card to homepage if not logged in

--- app/app/pages/login/login.ts
+++ app/app/pages/login/login.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Main } from '../main/main';
 import { TabsPage } from '../tabs/tabs';
-import { Welcome } from '../welcome/welcome';
 
 @Component({
   templateUrl: 'build/pages/login/login.html',
