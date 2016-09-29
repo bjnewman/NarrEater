250250fc5789af71259964fe910e79d820cd9b1c
David Armbrust <david.armbrust@gmail.com>
2016-09-29 10:26:17 -0500

    Update user login form

--- app/app/pages/login/login.ts
+++ app/app/pages/login/login.ts
@@ -2,21 +2,38 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Main } from '../main/main';
 import { TabsPage } from '../tabs/tabs';
+import { ApiService } from '../../apiService';
+import 'rxjs/add/operator/map';
 
 @Component({
   templateUrl: 'build/pages/login/login.html',
 })
 export class Login {
   public rootPage: any;
+  public loggedIn: boolean;
 
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private api: ApiService) {
     this.rootPage = TabsPage;
+    this.loggedIn = false;
   }
 
-  login() {
-
-    this.rootPage = TabsPage;
-    this.navCtrl.push(TabsPage, {});
+  login(email: string, password: string) {
+    console.log('login pressed' + email)
+    var response = this.api.userAuthenticate(email, password)
+    response.subscribe(
+      res => {
+        if(res.json()['response']['login']){
+          this.loggedIn = true
+          this.rootPage = TabsPage;
+          this.navCtrl.push(TabsPage, {});
+        } else {
+          this.loggedIn = false
+        }
+      },
+      error => {
+        console.log("error: " + error)
+      }
+    )
   }
 
   guestLogin() {
